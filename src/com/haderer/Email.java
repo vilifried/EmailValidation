d4d4a23756bfb42eeb6e097da68f8086398003c6
Wilfried Haderer <w.haderer@gmx.net>
2021-10-09 22:28:43 +0200

    final commit

---
+++ src/com/haderer/Email.java
@@ -0,0 +1,30 @@
+package com.haderer;
+
+import java.util.ArrayList;
+
+public class Email {
+
+	private ArrayList<String> emails = new ArrayList<>();
+	private String match;
+
+	public void addEmail(String emailId) {
+		this.emails.add(emailId);
+	}
+
+	public void findEmailId(String emailId) {
+		for (String str : emails) {
+			if (str.contains(emailId)) {
+				this.match = str;
+				break;
+			}
+		}
+	}
+
+	public void printMsg() {
+		if (this.match == null) {
+			System.err.println("No email found");
+		} else {
+			System.out.print("Match found: " + this.match + "\n");
+		}
+	}
+}
--- src/com/haderer/Program.java
+++ src/com/haderer/Program.java
@@ -1,10 +1,36 @@
 package com.haderer;
 
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		boolean stop = false;
+		Email email = new Email();
+		Scanner scanner = new Scanner(System.in);
+
+		email.addEmail("whaderer@gmx.net");
+		email.addEmail("mary@gmail.com");
+		email.addEmail("freddy@yahoo.com");
+		email.addEmail("paula@hotmail.com");
+		email.addEmail("andrew@gmail.com");
+		email.addEmail("johnDoe@mail.com");
+		email.addEmail("kevin@gmx.net");
+		email.addEmail("sebastian@mobil.de");
+		email.addEmail("mattsimmons@yahoo.com");
+		email.addEmail("mathewmiller@gmail.com");
+
+		while (!stop) {
+			System.out.println("Enter your email ort type end");
+			String input = scanner.nextLine().toLowerCase();
+			if (input.toLowerCase().equals("end")) {
+				stop = true;
+				scanner.close();
+			} else {
+				email.findEmailId(input);
+				email.printMsg();
+			}
+		}
 	}
-
 }
